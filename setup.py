76e76855060722db857146c96c737ab674043ea7
Ben Dyer <ben.dyer@taguchimail.com>
2015-07-28 21:34:13 +1000

    Add classifiers, keywords and extra packages to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,19 @@ args = dict(
     name='uavcan',
     version='0.1',
     description='UAVCAN for Python',
-    packages=['uavcan', 'uavcan.dsdl'],
+    packages=['uavcan', 'uavcan.dsdl', 'uavcan.services', 'uavcan.monitors'],
     author='Pavel Kirienko',
     author_email='pavel.kirienko@gmail.com',
     url='http://uavcan.org',
-    license='MIT'
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=''
 )
 
 setup(**args)
