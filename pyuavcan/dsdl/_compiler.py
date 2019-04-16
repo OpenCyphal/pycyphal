8a3b5a45f4cae8e3cade021472165385ca160932
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-04-16 13:49:25 +0300

    Numpy arrays

--- pyuavcan/dsdl/_compiler.py
+++ pyuavcan/dsdl/_compiler.py
@@ -44,40 +44,24 @@ def generate_python_package_from_dsdl_namespace(package_parent_directory: _AnyPa
 
     env = generator._env        # https://github.com/UAVCAN/pydsdlgen/issues/20
 
-    env.tests['boolean'] = lambda x: isinstance(x, pydsdl.BooleanType)
-    env.tests['integer'] = lambda x: isinstance(x, pydsdl.IntegerType)
-    env.tests['float'] = lambda x: isinstance(x, pydsdl.FloatType)
-    env.tests['array'] = lambda x: isinstance(x, pydsdl.ArrayType)
-    env.tests['composite'] = lambda x: isinstance(x, pydsdl.CompositeType)
+    env.tests['boolean']        = lambda x: isinstance(x, pydsdl.BooleanType)
+    env.tests['integer']        = lambda x: isinstance(x, pydsdl.IntegerType)
+    env.tests['float']          = lambda x: isinstance(x, pydsdl.FloatType)
+    env.tests['array']          = lambda x: isinstance(x, pydsdl.ArrayType)
+    env.tests['fixed_array']    = lambda x: isinstance(x, pydsdl.FixedLengthArrayType)
+    env.tests['variable_array'] = lambda x: isinstance(x, pydsdl.VariableLengthArrayType)
+    env.tests['composite']      = lambda x: isinstance(x, pydsdl.CompositeType)
 
-    env.tests['padding'] = lambda x: isinstance(x, pydsdl.PaddingField)
+    env.tests['padding']    = lambda x: isinstance(x, pydsdl.PaddingField)
+    env.tests['uint8']      = lambda x: isinstance(x, pydsdl.UnsignedIntegerType) and x.bit_length == 8
 
-    env.filters['type_annotation'] = _dsdl_type_to_annotation
-    env.filters['id'] = _make_identifier
-    env.filters['pickle'] = _pickle_object
+    env.filters['id']                   = _make_identifier
+    env.filters['pickle']               = _pickle_object
+    env.filters['numpy_scalar_type']    = _numpy_scalar_type
 
     generator.generate_all()
 
     return pathlib.Path(package_parent_directory) / pathlib.Path(root_namespace_name)
-
-
-def _dsdl_type_to_annotation(t: pydsdl.Any) -> str:
-    if isinstance(t, pydsdl.BooleanType):
-        return 'bool'
-    elif isinstance(t, pydsdl.IntegerType):
-        return 'int'
-    elif isinstance(t, pydsdl.FloatType):
-        return 'float'
-    elif isinstance(t, pydsdl.ArrayType):
-        et = t.element_type
-        if isinstance(et, pydsdl.UnsignedIntegerType) and et.bit_length <= 8:
-            return 'bytes'
-        else:
-            return f'_List_[{_dsdl_type_to_annotation(et)}]'
-    elif isinstance(t, (pydsdl.StructureType, pydsdl.UnionType)):
-        return t.full_name
-    else:
-        raise ValueError(f"Don't know how to construct type annotation for {type(t).__name__}")
 
 
 def _make_identifier(a: pydsdl.Attribute) -> str:
@@ -88,6 +72,26 @@ def _pickle_object(x: typing.Any) -> str:
     pck: str = base64.b85encode(gzip.compress(pickle.dumps(x, protocol=4))).decode().strip()
     segment_gen = map(''.join, itertools.zip_longest(*([iter(pck)] * 100), fillvalue=''))
     return '\n'.join(repr(x) for x in segment_gen)
+
+
+def _numpy_scalar_type(t: pydsdl.Any) -> str:
+    def pick_width(w: int) -> int:
+        for o in [8, 16, 32, 64]:
+            if w <= o:
+                return o
+        raise ValueError(f'Invalid bit width: {w}')
+
+    if isinstance(t, pydsdl.BooleanType):
+        return f'_np_.bool'
+    elif isinstance(t, pydsdl.SignedIntegerType):
+        return f'_np_.int{pick_width(t.bit_length)}'
+    elif isinstance(t, pydsdl.UnsignedIntegerType):
+        return f'_np_.uint{pick_width(t.bit_length)}'
+    elif isinstance(t, pydsdl.FloatType):
+        return f'_np_.float{pick_width(t.bit_length)}'
+    else:
+        assert not isinstance(t, pydsdl.PrimitiveType), 'Forgot to handle some primitive types'
+        return f'_np_.object_'
 
 
 def _unittest_dsdl_compiler() -> None:
