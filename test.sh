10849c8b3322d3e71487caf90df7dfd188a538b0
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-06-02 22:16:42 +0300

    Full test fix: postpone typechecking until after the unit tests are finished because that allows us to also typecheck the generated packages

--- test.sh
+++ test.sh
@@ -2,33 +2,35 @@
 
 status=0
 
-# Static type checking
-export MYPYPATH=".test_dsdl_generated"
-if ! mypy --strict --config-file=setup.cfg pyuavcan tests .test_dsdl_generated
-then
-    status=1
-fi
-
-# Code style checking
-if ! pycodestyle --show-source pyuavcan tests
-then
-    status=1
-fi
-
-# Download the public regulated types - they are needed for testing
+# Downloading the public regulated types - they are needed for testing.
 if [[ ! -d public_regulated_data_types.cache ]]
 then
     git clone https://github.com/UAVCAN/public_regulated_data_types --branch=uavcan-v1.0 \
         public_regulated_data_types.cache || exit 1
 fi
 
-# Unit tests
+# Unit testing.
 # https://docs.pytest.org/en/latest/pythonpath.html#invoking-pytest-versus-python-m-pytest
 export PYTHONASYNCIODEBUG=1
 if coverage run --source pyuavcan,tests,.test_dsdl_generated -m pytest -v pyuavcan tests $@
 then
     coverage report
 else
+    status=1
+fi
+
+# Static type checking.
+# We postpone type checking until after the tests have run in order to be able to typecheck the
+# generated Python packages as well.
+export MYPYPATH=".test_dsdl_generated"
+if ! mypy --strict --config-file=setup.cfg pyuavcan tests .test_dsdl_generated
+then
+    status=1
+fi
+
+# PEP8 code style enforcement.
+if ! pycodestyle --show-source pyuavcan tests
+then
     status=1
 fi
 
