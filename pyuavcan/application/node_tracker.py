9307fadc0a0c13898819e9ccbe392f709a159d46
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-04-09 23:24:55 +0300

    NodeTracker tests

--- pyuavcan/application/node_tracker.py
+++ pyuavcan/application/node_tracker.py
@@ -28,9 +28,9 @@ The info is None until the node responds to the GetInfo request.
 """
 
 
-EventHandler = typing.Callable[[int, typing.Optional[Entry], typing.Optional[Entry]], None]
+UpdateHandler = typing.Callable[[int, typing.Optional[Entry], typing.Optional[Entry]], None]
 """
-Arguments: node-ID, old entry, new entry. See :meth:`NodeTracker.add_handler` for details.
+Arguments: node-ID, old entry, new entry. See :meth:`NodeTracker.add_update_handler` for details.
 """
 
 
@@ -50,23 +50,27 @@ class NodeTracker:
     If the local node is anonymous, the info request functionality will be automatically disabled;
     it will be re-enabled automatically if the local node is assigned a node-ID later.
 
+    The tracked node registry *does not include the local node*.
+    If the local node-ID is N, the registry will not contain an entry at key N unless there is a node-ID conflict
+    in the network.
+
     The class provides IoC events which are triggered on change.
     The collected data can also be accessed by direct polling synchronously.
     """
 
-    REQUEST_PRIORITY = pyuavcan.transport.Priority.OPTIONAL
+    GET_INFO_PRIORITY = pyuavcan.transport.Priority.OPTIONAL
     """
     The logic tolerates the loss of responses, hence the optional priority level.
     This way, we can retry without affecting high-priority communications.
     """
 
-    REQUEST_TIMEOUT = 5.0
+    DEFAULT_GET_INFO_TIMEOUT = 5.0
     """
-    The request timeout is larger than the default because the data is immutable (does not lose validity over time)
-    and the priority level is low which may cause delays.
+    The default request timeout is larger than the recommended default because the data is immutable
+    (does not lose validity over time) and the priority level is low which may cause delays.
     """
 
-    REQUEST_ATTEMPTS = 10
+    DEFAULT_GET_INFO_ATTEMPTS = 10
     """
     Abandon efforts if the remote node did not respond to GetInfo this many times.
     The counter will resume from scratch if the node is restarted or a new node under that node-ID is detected.
@@ -78,9 +82,45 @@ class NodeTracker:
 
         self._registry: typing.Dict[int, Entry] = {}
         self._offline_timers: typing.Dict[int, asyncio.TimerHandle] = {}
-        self._info_tasks: typing.Dict[int, asyncio.Task] = {}
+        self._info_tasks: typing.Dict[int, asyncio.Task[None]] = {}
 
-        self._handlers: typing.List[EventHandler] = []
+        self._update_handlers: typing.List[UpdateHandler] = []
+
+        self._get_info_timeout = self.DEFAULT_GET_INFO_TIMEOUT
+        self._get_info_attempts = self.DEFAULT_GET_INFO_ATTEMPTS
+
+    @property
+    def get_info_timeout(self) -> float:
+        """
+        Allows the user to override the default ``uavcan.node.GetInfo`` request timeout.
+        The value shall be a finite positive number.
+        """
+        return self._get_info_timeout
+
+    @get_info_timeout.setter
+    def get_info_timeout(self, value: float) -> None:
+        value = float(value)
+        if 0 < value < float('+inf'):
+            self._get_info_timeout = value
+        else:
+            raise ValueError(f'Invalid response timeout value: {value}')
+
+    @property
+    def get_info_attempts(self) -> int:
+        """
+        Allows the user to override the default ``uavcan.node.GetInfo`` request retry limit.
+        The value shall be a non-negative integer number.
+        The value of zero disables GetInfo requests completely.
+        """
+        return self._get_info_attempts
+
+    @get_info_attempts.setter
+    def get_info_attempts(self, value: int) -> None:
+        value = int(value)
+        if 0 <= value:
+            self._get_info_attempts = value
+        else:
+            raise ValueError(f'Invalid attempt limit: {value}')
 
     @property
     def registry(self) -> typing.Dict[int, Entry]:
@@ -100,12 +140,13 @@ class NodeTracker:
 
     def close(self) -> None:
         """
-        When closed the registry is emptied. This is to avoid accidental reliance on obsolete data.
+        When closed the registry is emptied and all handlers are removed.
+        This is to avoid accidental reliance on obsolete data.
         """
         _logger.debug('Closing %s', self)
         self._sub_heartbeat.close()
         self._registry.clear()
-        self._handlers.clear()
+        self._update_handlers.clear()
 
         for tm in self._offline_timers.values():
             tm.cancel()
@@ -115,11 +156,11 @@ class NodeTracker:
             tsk.cancel()
         self._info_tasks.clear()
 
-    def add_handler(self, handler: EventHandler) -> None:
+    def add_update_handler(self, handler: UpdateHandler) -> None:
         """
         Register a callable that will be invoked whenever the node registry is changed.
         The arguments are: node-ID, old entry, new entry.
-        The hook is invoked in the following cases with the specified arguments:
+        The handler is invoked in the following cases with the specified arguments:
 
         - New node appeared online. The old-entry is None. The new-entry info is None.
         - A known node went offline. The new-entry is None.
@@ -127,16 +168,17 @@ class NodeTracker:
         - A node responds to a ``uavcan.node.GetInfo`` request. Neither entry is None. The new-entry info is not None.
 
         Received Heartbeat messages change the registry as well, but they do not trigger the hook.
+        Handlers can be added and removed at any moment regardless of whether the instance is started.
         """
         if not callable(handler):  # pragma: no cover
             raise ValueError(f'Bad handler: {handler}')
-        self._handlers.append(handler)
+        self._update_handlers.append(handler)
 
-    def remove_handler(self, handler: EventHandler) -> None:
+    def remove_update_handler(self, handler: UpdateHandler) -> None:
         """
         Remove a previously added hook identified by referential equivalence. Behaves like :meth:`list.remove`.
         """
-        self._handlers.remove(handler)
+        self._update_handlers.remove(handler)
 
     async def _on_heartbeat(self, msg: Heartbeat, metadata: pyuavcan.transport.TransferFrom) -> None:
         node_id = metadata.source_node_id
@@ -194,33 +236,36 @@ class NodeTracker:
             _logger.debug('GetInfo task for node %s canceled', node_id)
 
     def _request_info(self, node_id: int) -> None:
-        async def attempt(client: pyuavcan.presentation.Client[GetInfo]) -> bool:
-            response = await client.call(GetInfo.Request())
-            if response is not None:
-                _logger.debug('GetInfo response: %s', response)
-                obj, _meta = response
-                assert isinstance(obj, GetInfo.Response)
-                old = self._registry[node_id]
-                new = Entry(old[0], obj)
-                self._registry[node_id] = new
-                self._notify(node_id, old, new)
-                return True
-            return False
+        async def attempt() -> bool:
+            client = self._presentation.make_client_with_fixed_service_id(GetInfo, node_id)
+            try:
+                client.priority = self.GET_INFO_PRIORITY
+                client.response_timeout = self._get_info_timeout
+                response = await client.call(GetInfo.Request())
+                if response is not None:
+                    _logger.debug('GetInfo response: %s', response)
+                    obj, _meta = response
+                    assert isinstance(obj, GetInfo.Response)
+                    old = self._registry[node_id]
+                    new = Entry(old[0], obj)
+                    self._registry[node_id] = new
+                    self._notify(node_id, old, new)
+                    return True
+                return False
+            finally:
+                client.close()
 
         async def worker() -> None:
             try:
                 _logger.debug('GetInfo task for node %s started', node_id)
-                client = self._presentation.make_client_with_fixed_service_id(GetInfo, node_id)
-                client.priority = self.REQUEST_PRIORITY
-                client.response_timeout = self.REQUEST_TIMEOUT
-                remaining_attempts = self.REQUEST_ATTEMPTS
+                remaining_attempts = self._get_info_attempts
                 while remaining_attempts > 0:
                     try:
-                        if await attempt(client):
+                        if await attempt():
                             break
                     except (pyuavcan.transport.OperationNotDefinedForAnonymousNodeError,
                             pyuavcan.presentation.RequestTransferIDVariabilityExhaustedError):
-                        await asyncio.sleep(self.REQUEST_TIMEOUT)  # Keep retrying forever.
+                        await asyncio.sleep(self._get_info_timeout)  # Keep retrying forever.
                     else:
                         remaining_attempts -= 1
                     _logger.debug('GetInfo task for node %s will retry; remaining attempts: %s',
@@ -228,6 +273,8 @@ class NodeTracker:
                 _logger.debug('GetInfo task for node %s is exiting', node_id)
             except asyncio.CancelledError:
                 raise
+            except pyuavcan.transport.ResourceClosedError:
+                _logger.debug(f'GetInfo task for node {node_id} is stopping because the transport is closed.')
             except Exception as ex:
                 _logger.exception(f'GetInfo task for node {node_id} has crashed: {ex}')
             del self._info_tasks[node_id]
@@ -238,7 +285,7 @@ class NodeTracker:
     def _notify(self, node_id: int, old_entry: typing.Optional[Entry], new_entry: typing.Optional[Entry]) -> None:
         assert isinstance(old_entry, Entry) or old_entry is None
         assert isinstance(new_entry, Entry) or new_entry is None
-        for eh in self._handlers:
+        for eh in self._update_handlers:
             try:
                 eh(node_id, old_entry, new_entry)
             except Exception as ex:
--- tests/application/__init__.py
+++ tests/application/__init__.py
@@ -3,3 +3,11 @@
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel.kirienko@zubax.com>
 #
+
+import typing
+import pyuavcan
+
+
+def get_transport(node_id: typing.Optional[int]) -> pyuavcan.transport.Transport:
+    from pyuavcan.transport.udp import UDPTransport
+    return UDPTransport(f'127.0.0.{node_id}/8') if node_id is not None else UDPTransport('127.255.255.255/8')
