4f3bda26534fba335d537c8d5601a3138a7a5c54
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-11-01 02:03:37 +0200

    Fix Windows?

--- tests/cli/_demo_basic_usage.py
+++ tests/cli/_demo_basic_usage.py
@@ -145,13 +145,13 @@ def _unittest_slow_cli_demo_basic_usage(
         run_cli_tool(
             '-v',
             'pub', '2345.uavcan.si.sample.temperature.Scalar.1.0', '{kelvin: 321.5}',
-            '--count=5', '--period=0.5', '--priority=slow',
+            '--count=5', '--period=1', '--priority=slow',
             '--heartbeat-fields={vendor_specific_status_code: 123}',
             *iface_option.make_cli_args(1),  # type: ignore
             timeout=10.0
         )
 
-        time.sleep(5.0)     # Time to sync up
+        time.sleep(1.0)     # Time to sync up
 
         out_sub_heartbeat = proc_sub_heartbeat.wait(2.0, interrupt=True)[1].splitlines()
         out_sub_temperature = proc_sub_temperature.wait(2.0, interrupt=True)[1].splitlines()
