890690b0566f4c9e5b29f3036870da936367494e
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-11-14 19:40:22 +0200

    Fix a time-sensitive test on Windows

--- test.sh
+++ test.sh
@@ -113,7 +113,6 @@ coverage report
 banner STATIC ANALYSIS
 
 # We typecheck after the tests have run in order to be able to typecheck the generated Python packages as well.
-# Also, dear mypy, please stuff your caching, it's fucking broken.
 rm -rf .mypy_cache/ &> /dev/null
 echo 'YOU SHALL NOT PASS' > .mypy_cache
 chmod 444 .mypy_cache
