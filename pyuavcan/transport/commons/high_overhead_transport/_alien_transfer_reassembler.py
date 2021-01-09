45e20957c26483d646619ee022aae1e67d45089d
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-01-10 01:12:47 +0200

    Correct the timeout treatment in the HOT alien reassembler to match the recommendations given in the Spec

--- pyuavcan/transport/commons/high_overhead_transport/_alien_transfer_reassembler.py
+++ pyuavcan/transport/commons/high_overhead_transport/_alien_transfer_reassembler.py
@@ -16,10 +16,8 @@ class AlienTransferReassembler:
     which are extracted from captured transport frames.
     """
 
-    MAX_TRANSFER_ID_TIMEOUT = 2.0
-    """
-    The transfer-ID timeout is auto-deduced but it is clamped to this upper limit.
-    """
+    _MAX_INTERVAL = 1.0
+    _TID_TIMEOUT_MULTIPLIER = 2.0  # TID = 2*interval as suggested in the Specification.
 
     _EXTENT_BYTES = 2 ** 32
     """
@@ -34,13 +32,13 @@ class AlienTransferReassembler:
             on_error_callback=self._register_reassembly_error,
         )
         self._last_transfer_monotonic: float = 0.0
-        self._transfer_id_timeout = float(AlienTransferReassembler.MAX_TRANSFER_ID_TIMEOUT)
+        self._interval = float(AlienTransferReassembler._MAX_INTERVAL)
 
     def process_frame(
         self, timestamp: Timestamp, frame: Frame
     ) -> typing.Union[TransferFrom, TransferReassembler.Error, None]:
         trf = self._reassembler.process_frame(
-            timestamp=timestamp, frame=frame, transfer_id_timeout=self._transfer_id_timeout
+            timestamp=timestamp, frame=frame, transfer_id_timeout=self.transfer_id_timeout
         )
         if trf is None:
             out, self._last_error = self._last_error, None
@@ -48,8 +46,8 @@ class AlienTransferReassembler:
 
         # Update the transfer-ID timeout.
         delta = float(trf.timestamp.monotonic) - self._last_transfer_monotonic
-        delta = min(AlienTransferReassembler.MAX_TRANSFER_ID_TIMEOUT, max(0.0, delta))
-        self._transfer_id_timeout = (self._transfer_id_timeout + delta) * 0.5
+        delta = min(AlienTransferReassembler._MAX_INTERVAL, max(0.0, delta))
+        self._interval = (self._interval + delta) * 0.5
         self._last_transfer_monotonic = float(trf.timestamp.monotonic)
 
         return trf
@@ -60,7 +58,7 @@ class AlienTransferReassembler:
         The current value of the auto-deduced transfer-ID timeout.
         It is automatically adjusted whenever a new transfer is received.
         """
-        return self._transfer_id_timeout
+        return self._interval * AlienTransferReassembler._TID_TIMEOUT_MULTIPLIER
 
     def _register_reassembly_error(self, error: TransferReassembler.Error) -> None:
         self._last_error = error
