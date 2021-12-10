89656dd643a57e23c6674d3a5c809aabe639c2cc
Pavel Kirienko <pavel.kirienko@gmail.com>
2021-12-10 17:26:01 +0200

    Make SocketCAN test resilient against frame reordering

--- tests/transport/can/media/_socketcan.py
+++ tests/transport/can/media/_socketcan.py
@@ -65,9 +65,9 @@ async def _unittest_can_socketcan() -> None:
     ts_begin = Timestamp.now()
     await media_a.send(
         [
-            Envelope(DataFrame(FrameFormat.EXTENDED, 0xBADC0FE, bytearray(range(8))), loopback=True),
-            Envelope(DataFrame(FrameFormat.EXTENDED, 0x12345678, bytearray(range(0))), loopback=False),
             Envelope(DataFrame(FrameFormat.BASE, 0x123, bytearray(range(6))), loopback=True),
+            Envelope(DataFrame(FrameFormat.EXTENDED, 0x1BADC0FE, bytearray(range(8))), loopback=True),
+            Envelope(DataFrame(FrameFormat.EXTENDED, 0x1FF45678, bytearray(range(0))), loopback=False),
         ],
         asyncio.get_event_loop().time() + 1.0,
     )
@@ -85,25 +85,25 @@ async def _unittest_can_socketcan() -> None:
     rx_external = [e.frame for t, e in rx_a if not e.loopback]
     assert len(rx_loopback) == 2 and len(rx_external) == 3
 
-    assert rx_loopback[0].identifier == 0xBADC0FE
-    assert rx_loopback[0].data == bytearray(range(8))
-    assert rx_loopback[0].format == FrameFormat.EXTENDED
+    assert rx_loopback[0].identifier == 0x123
+    assert rx_loopback[0].data == bytearray(range(6))
+    assert rx_loopback[0].format == FrameFormat.BASE
 
-    assert rx_loopback[1].identifier == 0x123
-    assert rx_loopback[1].data == bytearray(range(6))
-    assert rx_loopback[1].format == FrameFormat.BASE
+    assert rx_loopback[1].identifier == 0x1BADC0FE
+    assert rx_loopback[1].data == bytearray(range(8))
+    assert rx_loopback[1].format == FrameFormat.EXTENDED
 
-    assert rx_external[0].identifier == 0xBADC0FE
-    assert rx_external[0].data == bytearray(range(8))
-    assert rx_external[0].format == FrameFormat.EXTENDED
+    assert rx_external[0].identifier == 0x123
+    assert rx_external[0].data == bytearray(range(6))
+    assert rx_external[0].format == FrameFormat.BASE
 
-    assert rx_external[1].identifier == 0x12345678
-    assert rx_external[1].data == bytearray(range(0))
+    assert rx_external[1].identifier == 0x1BADC0FE
+    assert rx_external[1].data == bytearray(range(8))
     assert rx_external[1].format == FrameFormat.EXTENDED
 
-    assert rx_external[2].identifier == 0x123
-    assert rx_external[2].data == bytearray(range(6))
-    assert rx_external[2].format == FrameFormat.BASE
+    assert rx_external[2].identifier == 0x1FF45678
+    assert rx_external[2].data == bytearray(range(0))
+    assert rx_external[2].format == FrameFormat.EXTENDED
 
     media_a.close()
     media_b.close()
