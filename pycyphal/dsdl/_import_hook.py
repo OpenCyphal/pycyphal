5d0a8a623ca1b5f1703357d783ccb9b8db7cb8b8
chemicstry <chemicstry@gmail.com>
2022-08-21 14:28:14 +0300

    Use a more fitting variable name

--- pycyphal/dsdl/_import_hook.py
+++ pycyphal/dsdl/_import_hook.py
@@ -105,9 +105,9 @@ def get_default_lookup_dirs() -> Sequence[str]:
 
 
 def get_default_output_dir() -> str:
-    pycyphal_home = os.environ.get("PYCYPHAL_PATH")
-    if pycyphal_home:
-        return pycyphal_home
+    pycyphal_path = os.environ.get("PYCYPHAL_PATH")
+    if pycyphal_path:
+        return pycyphal_path
     try:
         return str(pathlib.Path.home().joinpath(".pycyphal"))
     except RuntimeError as e:
