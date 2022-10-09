0c23a17a2d7d260163333a2c8854c6856361b72b
Maksim Drachov <maksim.drachov@outlook.com>
2022-10-09 17:58:21 +0300

    Added source_node_id to UDPFrame

--- pycyphal/transport/udp/_frame.py
+++ pycyphal/transport/udp/_frame.py
@@ -3,9 +3,12 @@
 # Author: Pavel Kirienko <pavel@opencyphal.org>
 
 from __future__ import annotations
+from multiprocessing.sharedctypes import Value
 import typing
 import struct
 import dataclasses
+
+from numpy import source
 import pycyphal
 
 
@@ -23,7 +26,7 @@ class UDPFrame(pycyphal.transport.commons.high_overhead_transport.Frame):
         struct Header {
             uint8_t  version;
             uint8_t  priority;
-            uint16_t _reserved_a;   // Set to zero when encoding, ignore when decoding.
+            uint16_t source_node_id;   
             uint32_t frame_index_eot;
             uint64_t transfer_id;
             uint64_t _reserved_b;   // Set to zero when encoding, ignore when decoding.
@@ -40,11 +43,23 @@ class UDPFrame(pycyphal.transport.commons.high_overhead_transport.Frame):
     +-----------------------------------------------+------------------------------------+
     """
 
-    _HEADER_FORMAT = struct.Struct("<BB2xIQ8x")
-    _VERSION = 0
+    _HEADER_FORMAT = struct.Struct(
+        "<" # little-endian
+        "B" # version
+        "B" # priority
+        "H" # source_node_id
+        "I" # frame_index_eot
+        "Q" # transfer_id
+        "8x" # reserved 64 bits
+    )
+    _VERSION = 1
 
     TRANSFER_ID_MASK = 2**64 - 1
+    SOURCE_NODE_ID_MASK = 2**16 - 1
     INDEX_MASK = 2**31 - 1
+
+    # No anonymous nodes for Cyphal/UDP
+    source_node_id: int
 
     def __post_init__(self) -> None:
         if not isinstance(self.priority, pycyphal.transport.Priority):
@@ -55,6 +70,9 @@ class UDPFrame(pycyphal.transport.commons.high_overhead_transport.Frame):
 
         if not (0 <= self.index <= self.INDEX_MASK):
             raise ValueError(f"Invalid frame index: {self.index}")
+
+        if not (0 <= self.source_node_id <= self.SOURCE_NODE_ID_MASK):
+            raise ValueError(f"Invalid source node id: {self.source_node_id}")
 
         if not isinstance(self.payload, memoryview):
             raise TypeError(f"Bad payload type: {type(self.payload).__name__}")  # pragma: no cover
@@ -67,20 +85,21 @@ class UDPFrame(pycyphal.transport.commons.high_overhead_transport.Frame):
         allowing the caller to rely on the vectorized IO API instead (sendmsg).
         """
         header = self._HEADER_FORMAT.pack(
-            self._VERSION, int(self.priority), self.index | ((1 << 31) if self.end_of_transfer else 0), self.transfer_id
+            self._VERSION, int(self.priority), self.source_node_id, self.index | ((1 << 31) if self.end_of_transfer else 0), self.transfer_id
         )
         return memoryview(header), self.payload
 
     @staticmethod
     def parse(image: memoryview) -> typing.Optional[UDPFrame]:
         try:
-            version, int_priority, frame_index_eot, transfer_id = UDPFrame._HEADER_FORMAT.unpack_from(image)
+            version, int_priority, source_node_id, frame_index_eot, transfer_id = UDPFrame._HEADER_FORMAT.unpack_from(image)
         except struct.error:
             return None
         if version == UDPFrame._VERSION:
             # noinspection PyArgumentList
             return UDPFrame(
                 priority=pycyphal.transport.Priority(int_priority),
+                source_node_id = source_node_id,
                 transfer_id=transfer_id,
                 index=(frame_index_eot & UDPFrame.INDEX_MASK),
                 end_of_transfer=bool(frame_index_eot & (UDPFrame.INDEX_MASK + 1)),
@@ -96,22 +115,27 @@ def _unittest_udp_frame_compile() -> None:
     from pycyphal.transport import Priority
     from pytest import raises
 
-    _ = UDPFrame(priority=Priority.LOW, transfer_id=0, index=0, end_of_transfer=False, payload=memoryview(b""))
+    _ = UDPFrame(priority=Priority.LOW, source_node_id=1, transfer_id=0, index=0, end_of_transfer=False, payload=memoryview(b""))
 
     with raises(ValueError):
         _ = UDPFrame(
-            priority=Priority.LOW, transfer_id=2**64, index=0, end_of_transfer=False, payload=memoryview(b"")
+            priority=Priority.LOW, source_node_id=1, transfer_id=2**64, index=0, end_of_transfer=False, payload=memoryview(b"")
         )
 
     with raises(ValueError):
         _ = UDPFrame(
-            priority=Priority.LOW, transfer_id=0, index=2**31, end_of_transfer=False, payload=memoryview(b"")
+            priority=Priority.LOW, source_node_id=1, transfer_id=0, index=2**31, end_of_transfer=False, payload=memoryview(b"")
+        )
+    
+    with raises(ValueError):
+        _ = UDPFrame(
+            priority=Priority.LOW, source_node_id=2**16, transfer_id=0, index=0, end_of_transfer=False, payload=memoryview(b"")
         )
 
     # Multi-frame, not the end of the transfer.
     assert (
         memoryview(
-            b"\x00\x06\x00\x00"
+            b"\x01\x06\x01\x00"
             b"\r\xf0\xdd\x00"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -119,6 +143,7 @@ def _unittest_udp_frame_compile() -> None:
         memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame(
         priority=Priority.SLOW,
+        source_node_id=1,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0x_0DD_F00D,
         end_of_transfer=False,
@@ -128,7 +153,7 @@ def _unittest_udp_frame_compile() -> None:
     # Multi-frame, end of the transfer.
     assert (
         memoryview(
-            b"\x00\x07\x00\x00"
+            b"\x01\x07\x02\x00"
             b"\r\xf0\xdd\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -136,6 +161,7 @@ def _unittest_udp_frame_compile() -> None:
         memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame(
         priority=Priority.OPTIONAL,
+        source_node_id=2,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0x_0DD_F00D,
         end_of_transfer=True,
@@ -145,7 +171,7 @@ def _unittest_udp_frame_compile() -> None:
     # Single-frame.
     assert (
         memoryview(
-            b"\x00\x00\x00\x00"
+            b"\x01\x00\x03\x00"
             b"\x00\x00\x00\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -153,6 +179,7 @@ def _unittest_udp_frame_compile() -> None:
         memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame(
         priority=Priority.EXCEPTIONAL,
+        source_node_id=3,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0,
         end_of_transfer=True,
@@ -169,13 +196,14 @@ def _unittest_udp_frame_parse() -> None:
     # Multi-frame, not the end of the transfer.
     assert UDPFrame(
         priority=Priority.SLOW,
+        source_node_id=4,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0x_0DD_F00D,
         end_of_transfer=False,
         payload=memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame.parse(
         memoryview(
-            b"\x00\x06\x00\x00"
+            b"\x01\x06\x04\x00"
             b"\r\xf0\xdd\x00"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -186,13 +214,14 @@ def _unittest_udp_frame_parse() -> None:
     # Multi-frame, end of the transfer.
     assert UDPFrame(
         priority=Priority.OPTIONAL,
+        source_node_id=5,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0x_0DD_F00D,
         end_of_transfer=True,
         payload=memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame.parse(
         memoryview(
-            b"\x00\x07\x00\x00"
+            b"\x01\x07\x05\x00"
             b"\r\xf0\xdd\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -203,13 +232,14 @@ def _unittest_udp_frame_parse() -> None:
     # Single-frame.
     assert UDPFrame(
         priority=Priority.EXCEPTIONAL,
+        source_node_id=6,
         transfer_id=0x_DEAD_BEEF_C0FFEE,
         index=0,
         end_of_transfer=True,
         payload=memoryview(b"Well, I got here the same way the coin did."),
     ) == UDPFrame.parse(
         memoryview(
-            b"\x00\x00\x00\x00"
+            b"\x01\x00\x06\x00"
             b"\x00\x00\x00\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -220,7 +250,7 @@ def _unittest_udp_frame_parse() -> None:
     # Too short.
     assert None is UDPFrame.parse(
         memoryview(
-            b"\x00\x07\x00\x00"
+            b"\x01\x07\x00\x00"
             b"\r\xf0\xdd\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
@@ -229,7 +259,7 @@ def _unittest_udp_frame_parse() -> None:
     # Bad version.
     assert None is UDPFrame.parse(
         memoryview(
-            b"\x01\x07\x00\x00"
+            b"\x02\x07\x00\x00"
             b"\r\xf0\xdd\x80"
             b"\xee\xff\xc0\xef\xbe\xad\xde\x00"
             b"\x00\x00\x00\x00\x00\x00\x00\x00"
