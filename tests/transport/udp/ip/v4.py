e7e1e58144ab3021c52e5e4e5b9262d9af5c4b1d
Maksim Drachov <maksim.drachov@outlook.com>
2022-10-29 15:06:53 +0300

    fix _unittest_socket_factory

--- tests/transport/udp/ip/v4.py
+++ tests/transport/udp/ip/v4.py
@@ -24,26 +24,26 @@ def _unittest_socket_factory() -> None:
 
     is_linux = sys.platform.startswith("linux")
 
-    fac = SocketFactory.new(IPv4Address("127.42.1.200"))
+    fac = SocketFactory.new(IPv4Address("127.40.1.200"))
     assert fac.max_nodes == 0xFFFF
-    assert str(fac.local_ip_address) == "127.42.1.200"
+    assert str(fac.local_ip_address) == "127.40.1.200"
 
     # SERVICE SOCKET TEST (unicast)
     ds = ServiceDataSpecifier(100, ServiceDataSpecifier.Role.REQUEST)
     test_u = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    test_u.bind(("127.42.0.123", service_data_specifier_to_udp_port(ds)))
+    test_u.bind(("127.40.0.123", service_data_specifier_to_udp_port(ds)))
 
     srv_o = fac.make_output_socket(123, ds)
     srv_o.send(b"Goose")
     rx = test_u.recvfrom(1024)
     assert rx[0] == b"Goose"
-    assert rx[1][0] == "127.42.1.200"
+    assert rx[1][0] == "127.40.1.200"
 
     srv_i = fac.make_input_socket(ds)
-    test_u.sendto(b"Duck", ("127.42.1.200", service_data_specifier_to_udp_port(ds)))
+    test_u.sendto(b"Duck", ("127.40.1.200", service_data_specifier_to_udp_port(ds)))
     rx = srv_i.recvfrom(1024)
     assert rx[0] == b"Duck"
-    assert rx[1][0] == "127.42.0.123"
+    assert rx[1][0] == "127.40.0.123"
     test_u.close()
 
     # MESSAGE SOCKET TEST (multicast)
@@ -52,25 +52,25 @@ def _unittest_socket_factory() -> None:
     # So we set up separate sockets for input and output.
     test_i = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     test_i.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    test_i.bind(("239.42.2.100" * is_linux, SUBJECT_PORT))
+    test_i.bind(("239.40.2.100" * is_linux, SUBJECT_PORT))
     test_i.setsockopt(
-        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.42.2.100") + socket.inet_aton("127.42.0.123")
+        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.40.2.100") + socket.inet_aton("127.40.0.123")
     )
 
     msg_o = fac.make_output_socket(None, MessageDataSpecifier(612))  # 612 = (2 << 8) + 100
     msg_o.send(b"Eagle")
     rx = test_i.recvfrom(1024)
     assert rx[0] == b"Eagle"
-    assert rx[1][0] == "127.42.1.200"
+    assert rx[1][0] == "127.40.1.200"
 
     test_o = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    test_o.bind(("127.42.0.123", 0))
-    test_o.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton("127.42.0.123"))
+    test_o.bind(("127.40.0.123", 0))
+    test_o.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton("127.40.0.123"))
     msg_i = fac.make_input_socket(MessageDataSpecifier(612))
-    test_o.sendto(b"Seagull", ("239.42.2.100", SUBJECT_PORT))
+    test_o.sendto(b"Seagull", ("239.40.2.100", SUBJECT_PORT))
     rx = msg_i.recvfrom(1024)
     assert rx[0] == b"Seagull"
-    assert rx[1][0] == "127.42.0.123"  # Same address we just bound to.
+    assert rx[1][0] == "127.40.0.123"  # Same address we just bound to.
 
     # ERRORS
     with raises(InvalidMediaConfigurationError):
