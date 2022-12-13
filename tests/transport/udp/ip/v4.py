86eedbf3c2ad9a5d6126d7b449738364da2c5fe3
Maksim Drachov <maksim.drachov@outlook.com>
2022-12-13 18:20:01 +0200

    _unittest_socket_factory update

--- tests/transport/udp/ip/v4.py
+++ tests/transport/udp/ip/v4.py
@@ -12,7 +12,7 @@ import socket
 from pycyphal.transport import MessageDataSpecifier, ServiceDataSpecifier, UnsupportedSessionConfigurationError
 from pycyphal.transport import InvalidMediaConfigurationError, Timestamp
 from pycyphal.transport.udp._ip._socket_factory import SocketFactory
-from pycyphal.transport.udp._ip._endpoint_mapping import SUBJECT_PORT, service_data_specifier_to_udp_port
+from pycyphal.transport.udp._ip._endpoint_mapping import DESTINATION_PORT
 from pycyphal.transport.udp._ip._v4 import SnifferIPv4, IPv4SocketFactory
 from pycyphal.transport.udp._ip import LinkLayerCapture
 from pycyphal.transport.udp import IPPacket, LinkLayerPacket, UDPIPPacket
@@ -22,20 +22,19 @@ def _unittest_socket_factory() -> None:
     from pytest import raises
     from ipaddress import IPv4Address
 
-    is_linux = sys.platform.startswith("linux")
+    is_linux = sys.platform.startswith("linux") or sys.platform.startswith("darwin")
 
-    fac = SocketFactory.new(IPv4Address("127.0.0.1"), subnet_id=13)
+    fac = SocketFactory.new(IPv4Address("127.0.0.1"))
     assert fac.max_nodes == 0xFFFF
     assert str(fac.local_ip_address) == "127.0.0.1"
-    assert fac.subnet_id == 13
 
     # SERVICE SOCKET TEST
     ds = ServiceDataSpecifier(100, ServiceDataSpecifier.Role.REQUEST)
     test_srv_i = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     test_srv_i.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    test_srv_i.bind(("239.53.1.200" * is_linux, service_data_specifier_to_udp_port(ds)))
+    test_srv_i.bind(("239.1.1.200" * is_linux, DESTINATION_PORT))
     test_srv_i.setsockopt(
-        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.53.1.200") + socket.inet_aton("127.0.0.1")
+        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.1.1.200") + socket.inet_aton("127.0.0.1")
     )
 
     srv_o = fac.make_output_socket(456, ds)
@@ -49,7 +48,7 @@ def _unittest_socket_factory() -> None:
     test_srv_o.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton("127.0.0.1"))
 
     srv_i = fac.make_input_socket(456, ds)
-    test_srv_o.sendto(b"Duck", ("239.53.1.200", service_data_specifier_to_udp_port(ds)))
+    test_srv_o.sendto(b"Duck", ("239.1.1.200", DESTINATION_PORT))
     time.sleep(1)
     rx = srv_i.recvfrom(1024)
     assert rx[0] == b"Duck"
@@ -61,9 +60,9 @@ def _unittest_socket_factory() -> None:
     # So we set up separate sockets for input and output.
     test_msg_i = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     test_msg_i.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    test_msg_i.bind(("239.52.2.100" * is_linux, SUBJECT_PORT))
+    test_msg_i.bind(("239.0.2.100" * is_linux, DESTINATION_PORT))
     test_msg_i.setsockopt(
-        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.52.2.100") + socket.inet_aton("127.0.0.1")
+        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton("239.0.2.100") + socket.inet_aton("127.0.0.1")
     )
 
     msg_o = fac.make_output_socket(None, MessageDataSpecifier(612))
@@ -77,7 +76,7 @@ def _unittest_socket_factory() -> None:
     test_msg_o.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton("127.0.0.1"))
 
     msg_i = fac.make_input_socket(None, MessageDataSpecifier(612))
-    test_msg_o.sendto(b"Seagull", ("239.52.2.100", SUBJECT_PORT))
+    test_msg_o.sendto(b"Seagull", ("239.0.2.100", DESTINATION_PORT))
     time.sleep(1)
     rx = msg_i.recvfrom(1024)
     assert rx[0] == b"Seagull"
@@ -85,22 +84,20 @@ def _unittest_socket_factory() -> None:
 
     # ERRORS
     with raises(InvalidMediaConfigurationError):
-        IPv4SocketFactory(IPv4Address("1.2.3.4"), 13).make_input_socket(
+        IPv4SocketFactory(IPv4Address("1.2.3.4")).make_input_socket(
             456, ServiceDataSpecifier(0, ServiceDataSpecifier.Role.RESPONSE)
         )
     with raises(InvalidMediaConfigurationError):
-        IPv4SocketFactory(IPv4Address("1.2.3.4"), 13).make_input_socket(None, MessageDataSpecifier(0))
+        IPv4SocketFactory(IPv4Address("1.2.3.4")).make_input_socket(None, MessageDataSpecifier(0))
     with raises(InvalidMediaConfigurationError):
-        IPv4SocketFactory(IPv4Address("1.2.3.4"), 13).make_output_socket(
+        IPv4SocketFactory(IPv4Address("1.2.3.4")).make_output_socket(
             1, ServiceDataSpecifier(0, ServiceDataSpecifier.Role.RESPONSE)
         )
     with raises(InvalidMediaConfigurationError):
-        IPv4SocketFactory(IPv4Address("1.2.3.4"), 13).make_output_socket(1, MessageDataSpecifier(0))
+        IPv4SocketFactory(IPv4Address("1.2.3.4")).make_output_socket(1, MessageDataSpecifier(0))
 
-    with raises(UnsupportedSessionConfigurationError):
+    with raises(AssertionError):
         fac.make_output_socket(1, MessageDataSpecifier(0))
-    with raises(UnsupportedSessionConfigurationError):
-        fac.make_output_socket(None, ServiceDataSpecifier(0, ServiceDataSpecifier.Role.RESPONSE))
 
     # CLEAN UP
     # test_u.close()
