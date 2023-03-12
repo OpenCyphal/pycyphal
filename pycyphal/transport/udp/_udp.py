5fd6bc3e20e92d8a8a65f548b6af0fee2b6e5ab7
Maksim Drachov <maksim.drachov@outlook.com>
2023-03-12 20:13:18 +0200

    some future shit

--- pycyphal/transport/udp/_udp.py
+++ pycyphal/transport/udp/_udp.py
@@ -2,6 +2,7 @@
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel@opencyphal.org>
 
+from __future__ import annotations
 import typing
 import asyncio
 import logging
