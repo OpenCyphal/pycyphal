15aecc92fe5c9d696e41f7d6154e6d008fd21d16
Maksim Drachov <maksim.drachov@outlook.com>
2023-04-15 16:30:38 +0300

    removed 2 extensions

--- docs/conf.py
+++ docs/conf.py
@@ -82,8 +82,6 @@ extensions = [
     "sphinx.ext.graphviz",
     "sphinx_computron",
     "ref_fixer_hack",
-    "sphinxcontrib.mermaid",
-    "sphinxcontrib.contentui",
 ]
 sys.path.append(str(DOC_ROOT))  # This is for the hack to be importable
 
