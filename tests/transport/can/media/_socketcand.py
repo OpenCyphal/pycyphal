ccb9ef2978889d5bbc496243d9db14b878dd7c19
wiboticanders <anders.pearson@wibotic.com>
2023-08-29 17:26:51 -0700

    Updated test environment and tests
    Test environment now properly builds and breaks down socketcand
    Tests use rx_a and rx_b for proper communication

--- tests/transport/can/media/_socketcand.py
+++ tests/transport/can/media/_socketcand.py
@@ -22,23 +22,18 @@ should a test go crazy and eat all memory.
 """
 
 _logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+
+socketcand = None
 
 
 @pytest.fixture()
-def configure_host_environment() -> None:
+def _configure_host_environment() -> None:
     print("configurng host environment")
 
     def execute(
         *cmd: typing.Any, ensure_success: bool = True, cwd: typing.Optional[str] = None, daemon: bool = False
     ) -> typing.Tuple[int, str, str]:
         cmd = tuple(map(str, cmd))
-        if daemon:
-            subprocess.Popen(
-                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-            )  # start subproccess without waiting for output
-            return 0
-
         out = None
         if cwd is None:
             out = subprocess.run(  # pylint: disable=subprocess-run-check
@@ -82,30 +77,38 @@ def configure_host_environment() -> None:
         execute("make", cwd="socketcand")
         execute("sudo", "make", "install", cwd="socketcand")
 
-        subprocess.Popen(["socketcand", "-i", "vcan3", "-l", "lo"])
+        # start socketcand daemon in background so test can continue
+        socketcand = subprocess.Popen(["socketcand", "-i", "vcan3", "-l", "lo"])
 
-        # execute("socketcand", "-i", "vcan3", "-l", "lo", cwd = None, daemon=True)
-        # execute("socketcand", "-i", "vcan3", "-l", "lo", "-p", "8080")
+    yield None
+    if sys.platform.startswith("linux"):
+        socketcand.kill()
+        execute("sudo", "rm", "-r", "socketcand")
 
 
 @pytest.mark.asyncio
-async def _unittest_can_socketcand(configure_host_environment) -> None:
+async def _unittest_can_socketcand(_configure_host_environment) -> None:
     asyncio.get_running_loop().slow_callback_duration = 5.0
 
     media_a = SocketcandMedia("vcan3", "127.0.0.1")
-    asyncio.sleep(2.0)
+    media_b = SocketcandMedia("vcan3", "127.0.0.1")
 
-    assert media_a.interface_name == "socketcand"
-    assert media_a.channel_name == "vcan3"
-    assert media_a.host_name == "127.0.0.1"
-    assert media_a.port_name == 29536
     assert media_a.mtu == 8
-    assert media_a.number_of_acceptance_filters == 1
-    assert media_a._maybe_thread is None  # pylint: disable=protected-access
-
-    media_a.configure_acceptance_filters([FilterConfiguration.new_promiscuous()])
+    assert media_b.mtu == 8
+    assert media_a.interface_name == "socketcand"
+    assert media_b.interface_name == "socketcand"
+    assert media_a.channel_name == "vcan3"
+    assert media_b.channel_name == "vcan3"
+    assert media_a.host_name == "127.0.0.1"
+    assert media_b.host_name == "127.0.0.1"
+    assert media_a.port_name == 29536
+    assert media_b.port_name == 29536
+    assert media_a.number_of_acceptance_filters == media_b.number_of_acceptance_filters
+    assert media_a._maybe_thread is None
+    assert media_b._maybe_thread is None
 
     rx_a: typing.List[typing.Tuple[Timestamp, Envelope]] = []
+    rx_b: typing.List[typing.Tuple[Timestamp, Envelope]] = []
 
     def on_rx_a(frames: typing.Iterable[typing.Tuple[Timestamp, Envelope]]) -> None:
         nonlocal rx_a
@@ -113,41 +116,66 @@ async def _unittest_can_socketcand(configure_host_environment) -> None:
         print("RX A:", frames)
         rx_a += frames
 
-    media_a.start(on_rx_a, False)
+    def on_rx_b(frames: typing.Iterable[typing.Tuple[Timestamp, Envelope]]) -> None:
+        nonlocal rx_b
+        frames = list(frames)
+        print("RX B:", frames)
+        rx_b += frames
 
-    assert media_a._maybe_thread is not None  # pylint: disable=protected-access
-    await asyncio.sleep(2.0)  # This wait is needed to ensure that the RX thread handles select() timeout properly
+    media_a.start(on_rx_a, False)
+    media_b.start(on_rx_b, False)
+
+    assert media_a._maybe_thread is not None
+    assert media_b._maybe_thread is not None
+
+    await asyncio.sleep(2.0)  # This wait is needed to ensure that the RX thread handles read timeout properly
 
     ts_begin = Timestamp.now()
-    await media_a.send(
+    await media_b.send(
         [
+            Envelope(DataFrame(FrameFormat.EXTENDED, 0xBADC0FE, bytearray(range(8))), loopback=True),
+            Envelope(DataFrame(FrameFormat.EXTENDED, 0x12345678, bytearray(range(0))), loopback=False),
             Envelope(DataFrame(FrameFormat.BASE, 0x123, bytearray(range(6))), loopback=True),
         ],
         asyncio.get_event_loop().time() + 1.0,
     )
-    
-    await asyncio.sleep(1.0)
+    await asyncio.sleep(0.1)
     ts_end = Timestamp.now()
 
     print("rx_a:", rx_a)
-    # Three sent back from the other end, two loopback
-    assert len(rx_a) == 2
-    for t, _ in rx_a:
-        assert ts_begin.monotonic_ns <= t.monotonic_ns <= ts_end.monotonic_ns
-        assert ts_begin.system_ns <= t.system_ns <= ts_end.system_ns
+    # Three received from another part
+    assert len(rx_a) == 3
+    for ts, _f in rx_a:
+        assert ts_begin.monotonic_ns <= ts.monotonic_ns <= ts_end.monotonic_ns
+        assert ts_begin.system_ns <= ts.system_ns <= ts_end.system_ns
 
-    rx_loopback = [e.frame for t, e in rx_a if e.loopback]
-    rx_external = [e.frame for t, e in rx_a if not e.loopback]
-    assert len(rx_loopback) == 1 and len(rx_external) == 1
+    rx_external = list(filter(lambda x: True, rx_a))
 
-    assert rx_loopback[0].identifier == 0x123
-    assert rx_loopback[0].data == bytearray(range(6))
-    assert rx_loopback[0].format == FrameFormat.BASE
+    assert rx_external[0][1].frame.identifier == 0xBADC0FE
+    assert rx_external[0][1].frame.data == bytearray(range(8))
+    assert rx_external[0][1].frame.format == FrameFormat.EXTENDED
 
-    assert rx_external[0].identifier == 0x123
-    assert rx_external[0].data == bytearray(range(6))
-    assert rx_external[0].format == FrameFormat.BASE
+    assert rx_external[1][1].frame.identifier == 0x12345678
+    assert rx_external[1][1].frame.data == bytearray(range(0))
+    assert rx_external[1][1].frame.format == FrameFormat.EXTENDED
+
+    assert rx_external[2][1].frame.identifier == 0x123
+    assert rx_external[2][1].frame.data == bytearray(range(6))
+    assert rx_external[2][1].frame.format == FrameFormat.BASE
+
+    print("rx_b:", rx_b)
+    # Two messages are loopback and were copied
+    assert len(rx_b) == 2
+
+    rx_loopback = list(filter(lambda x: True, rx_b))
+
+    assert rx_loopback[0][1].frame.identifier == 0xBADC0FE
+    assert rx_loopback[0][1].frame.data == bytearray(range(8))
+    assert rx_loopback[0][1].frame.format == FrameFormat.EXTENDED
+
+    assert rx_loopback[1][1].frame.identifier == 0x123
+    assert rx_loopback[1][1].frame.data == bytearray(range(6))
+    assert rx_loopback[1][1].frame.format == FrameFormat.BASE
 
     media_a.close()
-
-    await asyncio.sleep(1)  # Let all pending tasks finalize properly to avoid stack traces in the output.
+    media_b.close()
