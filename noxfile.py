4620d7424f87f85ea5fec47b7b19d63f6506786f
Pavel Kirienko <pavel.kirienko@gmail.com>
2025-05-11 22:21:40 +0300

    ci

--- noxfile.py
+++ noxfile.py
@@ -75,6 +75,9 @@ def test(session):
         session.run("sudo", "setcap", "cap_net_raw+eip", str(Path(session.bin, "python").resolve()), external=True)
 
     # Launch the TCP broker for testing the Cyphal/serial transport.
+    print(f"{os.environ=}")
+    print(f"{session.env=}")
+    os.system("ncat --version")
     broker_process = subprocess.Popen(
         ["ncat", "--broker", "--listen", "-p", "50905"],
         env={k: (v or "") for k, v in session.env.items()},
