eca5ee46e2ce39c160a530ed7b43f5f3dc69b3bf
Huong Pham <email.me@0w.ee>
2025-06-03 01:06:55 +0300

    add test

--- tests/dsdl/_compiler.py
+++ tests/dsdl/_compiler.py
@@ -1,13 +1,16 @@
 # Copyright (c) 2020 OpenCyphal
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel@opencyphal.org>
-
+import sys
 import typing
 import logging
 import pathlib
 import tempfile
 import pytest
 import pycyphal.dsdl
+from pycyphal.dsdl._import_hook import DsdlMetaFinder
+from pycyphal.dsdl import remove_import_hooks, add_import_hook
+
 from .conftest import DEMO_DIR
 
 
@@ -37,6 +40,30 @@ def _unittest_module_import_path_usage_suggestion(caplog: typing.Any) -> None:
         output_directory.cleanup()  # This may fail on Windows with Python 3.7, we don't care.
     except PermissionError:  # pragma: no cover
         pass
+
+
+def _unittest_remove_import_hooks():
+    original_meta_path = sys.meta_path.copy()
+    try:
+        old_hooks = [hook for hook in sys.meta_path.copy() if isinstance(hook, DsdlMetaFinder)]
+        if not old_hooks:
+            print("No DsdlMetaFinder hooks found; nothing to test.")
+            return None
+
+        # call remove_import_hooks, make sure they disappear
+        remove_import_hooks()
+        current_hooks = [meta_path for meta_path in sys.meta_path.copy() if isinstance(meta_path, DsdlMetaFinder)]
+
+        assert not current_hooks, "Import hooks were not removed properly"
+
+        # Re-add the original hooks
+        add_import_hook()
+        final_hooks = [meta_path for meta_path in sys.meta_path.copy() if isinstance(meta_path, DsdlMetaFinder)]
+
+        assert old_hooks == final_hooks, "Hooks were not restored properly"
+        return None
+    finally:
+        sys.meta_path = original_meta_path
 
 
 def _unittest_issue_133() -> None:
