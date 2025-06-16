50a414b3d686de8844249ac84fff13ac5843e4e3
Huong Pham <email.me@0w.ee>
2025-06-16 15:00:31 +0300

    remove test related to old UX code

--- tests/dsdl/_compiler.py
+++ tests/dsdl/_compiler.py
@@ -6,8 +6,6 @@ import random
 import sys
 import threading
 import time
-import typing
-import logging
 import pathlib
 import tempfile
 import pytest
@@ -21,28 +19,6 @@ def _unittest_bad_usage() -> None:
     with pytest.raises(TypeError):
         # noinspection PyTypeChecker
         pycyphal.dsdl.compile("irrelevant", "irrelevant")  # type: ignore
-
-
-def _unittest_module_import_path_usage_suggestion(caplog: typing.Any) -> None:
-    caplog.set_level(logging.INFO)
-    output_directory = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
-    output_directory_name = pathlib.Path(output_directory.name).resolve()
-    caplog.clear()
-    pycyphal.dsdl.compile(DEMO_DIR / "public_regulated_data_types" / "uavcan", output_directory=output_directory.name)
-    logs = caplog.record_tuples
-    print("Captured log entries:", logs, sep="\n")
-    for e in logs:
-        if "dsdl" in e[0] and str(output_directory_name) in e[2]:
-            assert e[1] == logging.INFO
-            assert " path" in e[2]
-            assert "Path(" not in e[2]  # Ensure decent formatting
-            break
-    else:
-        assert False
-    try:
-        output_directory.cleanup()  # This may fail on Windows with Python 3.7, we don't care.
-    except PermissionError:  # pragma: no cover
-        pass
 
 
 def _unittest_remove_import_hooks() -> None:
